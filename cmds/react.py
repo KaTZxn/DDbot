d8f708340c95589415034840977a3f09646e9bc5
MODERN-14\katz0 <katz03010@gmail.com>
2021-09-10 17:49:04 +0900

    Update

--- cmds/react.py
+++ cmds/react.py
@@ -1,16 +1,23 @@
 import discord
+import random
 from discord.ext import commands
 from core.classes import Cog_Extension
 
 class React(Cog_Extension):
     
+    messageList = [
+        '躝啦屌你老母',
+        '唔該躝',
+        '躝啦'
+    ]
+
     @commands.Cog.listener()
     async def on_message(self,message):
         if message.author == self.bot.user:
             return
-        if message.content.startswith('躝'):
+        
             channel = message.channel
-            await channel.send('躝啦屌你老母')
+            await channel.send(self.messageList[random.randint(0,2)])
 
 def setup(bot):
     bot.add_cog(React(bot))
