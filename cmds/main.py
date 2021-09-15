d02b83d103f9991cfe058f85dfcd09c49dd8b19a
KaTZxn <73007704+KaTZxn@users.noreply.github.com>
2021-09-15 21:52:27 +0900

    OK

--- cmds/main.py
+++ cmds/main.py
@@ -3,26 +3,35 @@ from discord.ext import commands
 from core.classes import Cog_Extension
 import random
 
+
 class Main(Cog_Extension):
 
     @commands.command()
-    async def 躝(self,ctx):
+    async def 躝(self, ctx):
         await ctx.send('躝啦屌你老母')
 
     @commands.command()
-    async def hi(self,ctx):
+    async def hi(self, ctx):
         await ctx.send('屌你老母')
 
     @commands.command()
-    async def car(self,ctx,msg):
+    async def car(self, ctx, msg):
         link = ("https://nhentai.net/g/" + str(msg))
-        await ctx.send(link)    
+        await ctx.send(link)
 
     @commands.command()
-    async def randomcar(self,ctx):
-        num = random.randint(1,370000)
+    async def randomcar(self, ctx):
+        num = random.randint(1, 370000)
         link = ("https://nhentai.net/g/" + str(num))
-        await ctx.send(link)   
+        await ctx.send(link)
+
+    @commands.command()
+    async def 屌(self, ctx, msg):
+        if '@' in msg:
+            await ctx.send(msg+'屌你')
+        else:
+            await ctx.send('你想屌邊個啊屌你')
+
 
 def setup(bot):
     bot.add_cog(Main(bot))
--- cmds/react.py
+++ cmds/react.py
@@ -3,8 +3,9 @@ import random
 from discord.ext import commands
 from core.classes import Cog_Extension
 
+
 class React(Cog_Extension):
-    
+
     messageList = [
         '躝啦屌你老母',
         '唔該躝',
@@ -12,13 +13,25 @@ class React(Cog_Extension):
     ]
 
     @commands.Cog.listener()
-    async def on_message(self,message):
+    async def on_message(self, message):
         if message.author == self.bot.user:
             return
-        
+
         if '躝' in message.content:
             channel = message.channel
-            await channel.send(self.messageList[random.randint(0,2)])
+            await channel.send(self.messageList[random.randint(0, 2)])
+
+        gayList = [
+            '米高',
+            '木屌',
+            'Ag',
+            'Hei'
+        ]
+        for gay in gayList:
+            if gay in message.content:
+                channel = message.channel
+                await channel.send("He's gay.")
+
 
 def setup(bot):
     bot.add_cog(React(bot))
