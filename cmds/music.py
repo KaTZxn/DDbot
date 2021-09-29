94ace3fd5a51e5f4bbc2cce98a7959f1ca760fd1
KaTZxn <73007704+KaTZxn@users.noreply.github.com>
2021-09-30 02:29:34 +0900

    Update music.py

--- cmds/music.py
+++ cmds/music.py
@@ -10,12 +10,14 @@ class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+        # all the music related stuff
         self.is_playing = False
 
+        # 2d array containing [song, channel]
         self.music_queue = []
         self.YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
         self.FFMPEG_OPTIONS = {
-            'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': 'vn'}
+            'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
         self.vc = ""
 
