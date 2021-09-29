52d2644a0a344a6090b681df17f555a3d3f291fd
KaTZxn <73007704+KaTZxn@users.noreply.github.com>
2021-09-30 01:50:02 +0900

    Add bot

--- core/classes.py
+++ core/classes.py
@@ -1,6 +1,21 @@
 import discord
 from discord.ext import commands
 
+
 class Cog_Extension(commands.Cog):
-    def __init__(self,bot):
+    def __init__(self, bot):
         self.bot = bot
+
+
+class Music_Cog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        self.is_playing = False
+
+        self.music_queue = []
+        self.YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
+        self.FFMPEG_OPTIONS = {
+            'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': 'vn'}
+
+        self.vc = ""
