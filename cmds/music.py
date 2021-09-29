e50b5109e4c98f8d8b0990cf9381361e445be281
KaTZxn <73007704+KaTZxn@users.noreply.github.com>
2021-09-30 02:56:48 +0900

    Update music.py

--- cmds/music.py
+++ cmds/music.py
@@ -65,7 +65,7 @@ class Music(commands.Cog):
             self.music_queue.pop(0)
 
             self.vc.play(discord.FFmpegPCMAudio(
-                m_url, executable="ffmpeg/ffmpeg.exe", **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
+                m_url,  **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
         else:
             self.is_playing = False
 
