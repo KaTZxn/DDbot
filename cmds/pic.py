5e5e1d5113cbdd4dac0a1d517a6e18b9e4696f65
MODERN-14\katz0 <katz03010@gmail.com>
2021-10-15 13:49:58 +0900

    Update pic.py

--- cmds/pic.py
+++ cmds/pic.py
@@ -4,11 +4,12 @@ from core.classes import Cog_Extension
 import random
 from bs4 import BeautifulSoup
 import requests
+import time
 
 
 class Pic(Cog_Extension):
     @commands.command()
-    async def s(self, ctx, msg,*,num=1):
+    async def s(self, ctx, msg,*,num = 1):
         response = requests.get(f'https://yande.re/post?tags={msg}')
         soup = BeautifulSoup(response.text, "html.parser")
         results = soup.find_all('a', {'class': 'directlink largeimg'})
@@ -16,6 +17,20 @@ class Pic(Cog_Extension):
         for i in range(num):
             num = random.randint(1, len(image_links))
             await ctx.send(image_links[num])
+
+    @commands.command()
+    async def ts(self, ctx, msg,*,time = 5):
+        response = requests.get(f'https://yande.re/post?tags={msg}')
+        soup = BeautifulSoup(response.text, "html.parser")
+        results = soup.find_all('a', {'class': 'directlink largeimg'})
+        image_links = [result.get("href") for result in results]
+        num = random.randint(1, len(image_links))
+        pic = await ctx.send(image_links[num])
+        await asyncio.sleep(time)
+        await ctx.pic.delete()
+        
+
+
 
 
 def setup(bot):
