89a873c03459ef480322791977c53a035423f967
MODERN-14\katz0 <katz03010@gmail.com>
2021-10-15 09:47:41 +0900

    Update pic.py

--- cmds/pic.py
+++ cmds/pic.py
@@ -8,12 +8,12 @@ import requests
 
 class Pic(Cog_Extension):
     @commands.command()
-    async def s(self, ctx, msg,*,num):
+    async def s(self, ctx, msg,*,num=1):
         response = requests.get(f'https://yande.re/post?tags={msg}')
         soup = BeautifulSoup(response.text, "html.parser")
         results = soup.find_all('a', {'class': 'directlink largeimg'})
         image_links = [result.get("href") for result in results]
-        if num is None:
+        for i in num:
             num = random.randint(1, len(image_links))
             await ctx.send(image_links[num])
 
